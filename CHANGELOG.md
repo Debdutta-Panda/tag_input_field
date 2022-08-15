1411818e8115dfb07ac44c4d8d05e6b661eae28a
Debdutta Panda <me@debduttapanda.com>
2022-08-15 10:35:30 +0530

    Example added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.0.1
 
-* TODO: Describe initial release.
+- Put tag by pressing comma after tag content.
+- Put tag(s) by pasting comma separated contents.
+- Autocomplete feature.
+- Fully customizable. Almost every part can be customized.
+- delete tag(s) by backspace. Not possible in iOS with soft keyboard
---
+++ example/lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:tag_input_field/tag_input_field.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tag input field"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: SizedBox(
+          width: double.maxFinite,
+          child: TagInputField(
+            onBeforeTagAdded: (_)=>true,
+            onBeforeTagsAdded: (_)=>true,
+            onItemWillBeDeleted: (_)=>true,
+            onTagAdded: (_){},
+            onTagsAdded: (_){},
+            onTagDeleted: (_){},
+            autocomplete: true,
+            optionBuilder: (tev){
+              var text = tev.text;
+              if(text.isEmpty){
+                return [];
+              }
+              return [
+                "$text 1",
+                "$text 2",
+                "$text 3",
+                "$text 4",
+              ];
+            },
+          ),
+        ),
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
